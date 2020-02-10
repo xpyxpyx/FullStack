e1f85e07971357c19829a738e4cca3e2a9c13639
xpyxpyx <42948718+xpyxpyx@users.noreply.github.com>
2020-02-10 23:47:49 +0200

    Add files via upload

---
+++ osa3/build/precache-manifest.92404832e7e2f2acafeba57967c749f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29a6316c6cfe9603cdd4e714f1c26888",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f80e7af1dd8c4a520f85",
+    "url": "/static/css/main.2307a20c.chunk.css"
+  },
+  {
+    "revision": "4b18cf36501447135d88",
+    "url": "/static/js/2.7c9eebd5.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.7c9eebd5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f80e7af1dd8c4a520f85",
+    "url": "/static/js/main.af1004c1.chunk.js"
+  },
+  {
+    "revision": "cf9fd95ce589cae23005",
+    "url": "/static/js/runtime-main.2e9fb530.js"
+  }
+]);
